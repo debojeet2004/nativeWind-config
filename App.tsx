6f0407d35974ca7a5afde6ad7a3a891b5de3af8f
Debojeet Karmakar <debojeetk.cse.jisu21@gmail.com>
2024-07-26 22:38:52 +0530

    Reactnative-Nativewind config
    Signed-off-by: Debojeet Karmakar <debojeetk.cse.jisu21@gmail.com>

---
+++ App.tsx
@@ -0,0 +1,12 @@
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-red-500 flex justify-center items-center flex-1">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
