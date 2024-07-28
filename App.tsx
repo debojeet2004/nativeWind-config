442d0311cd3525e7a8831a68ccdbccd99ce8d985
Debojeet Karmakar <debojeetk.cse.jisu21@gmail.com>
2024-07-28 13:59:05 +0530

    minor changes
    Signed-off-by: Debojeet Karmakar <debojeetk.cse.jisu21@gmail.com>

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="bg-red-500 flex justify-center items-center flex-1">
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="bg-amber-700 flex justify-center items-center flex-1">
+      <Text className="text-white text-3xl">Open up App.tsx to start working on your app!</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
